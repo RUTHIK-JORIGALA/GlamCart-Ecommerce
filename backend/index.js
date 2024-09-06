ccb279fe356fc83fbba6e8319b056bc247f73606
RUTHIK-JORIGALA <104410015+RUTHIK-JORIGALA@users.noreply.github.com>
2024-09-06 15:57:56 +0530

    setting up backend

---
+++ backend/index.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const mongoose = require('mongoose');
+var cors = require('cors')
+const app = express()
+require('dotenv').config()
+const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
+
+const port = process.env.PORT || 3000;
+
+// middleware setup
+app.use(express.json({limit: "25mb"}));
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}))
+
+main()
+    .then(()=>{
+        console.log("Connected to MOngoDB")
+    })
+    .catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect(process.env.DB_URL);
+
+}
+
+app.get('/', (req, res) => {
+  res.send('Hello World! Welcome Raj')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
